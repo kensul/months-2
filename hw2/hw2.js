003faf42eccbc7a3b252c0b6518d9a125594f13f
kensul <sd.delkg@gmail.com>
2022-06-10 18:14:48 +0600

    hw2

--- hw2/hw2.js
+++ hw2/hw2.js
@@ -1,22 +1,11 @@
-const obj = {
-    star1: '*',
-    star2: '**',
-    star3: '***',
-    star4: '****',
-    star5: '*****',
-    star6: '******',
-    star7: '*******',
-}
-for(let star in obj){
-    console.log(obj[star])
-}
-
-
 let star = '*';
 while (star.length < 8) {
     console.log(star);
     star = star + '*'
 }
+
+
+
 
 for (let i = 1; i < 101; i++) {
     if (i % 15 === 0) console.log('FizzBuzz');
